de672dbc9315c41f19c6694d2aae7e1c26882aa2
Nayan Patidar <nayanpatidar28@gmail.com>
2023-08-02 22:05:14 +0530

    Added Librarian Class

--- src/main/java/Librarian/Librarian.java
+++ src/main/java/Librarian/Librarian.java
@@ -1,4 +1,47 @@
 package Librarian;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
 public class Librarian {
+    private String userName;
+
+    public Librarian(String userName){
+        this.userName = userName;
+        verifyLibrarian();
+    }
+
+    private void verifyLibrarian() {
+        ArrayList<LIBRARIAN_ID> librarianIds= new ArrayList<>();
+        librarianIds.add(new LIBRARIAN_ID("101", userName));
+        boolean ValidLibrarian = false;
+
+        Scanner scn = new Scanner(System.in);
+
+        do {
+            System.out.println("ENTER YOUR ID");
+            String ID = scn.nextLine();
+            System.out.println("Your password is in format id@name");
+
+            for (LIBRARIAN_ID val : librarianIds){
+                if (val.getName().equals(userName) && val.getID().equals(ID)){
+                    System.out.println("Present");
+                    ValidLibrarian = false;
+                }else{
+                    System.out.println("ENTER YOUR CREDENTIALS AGAIN !!! \n");
+                    try {
+                        TimeUnit.SECONDS.sleep(3);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }while (ValidLibrarian);
+
+
+
+    }
+
+
 }
--- src/main/java/UserManagementSystem/UserManagement.java
+++ src/main/java/UserManagementSystem/UserManagement.java
@@ -1,5 +1,7 @@
 package UserManagementSystem;
 
+import Librarian.Librarian;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -29,7 +31,7 @@ public class UserManagement {
                 }
                 else if (num == 2){
 //                TODO call user
-//              VerifyLibrarian();
+                 VerifyLibrarian();
                     System.out.println("Librarian");
                     ValidInput = true;
                 }
@@ -42,6 +44,33 @@ public class UserManagement {
                 }
             } catch (IOException e) {
                 System.out.println("Input Error");
+            }
+        }
+    }
+
+    private void VerifyLibrarian() {
+        ArrayList<USERNAME_PWD> LibrarianList = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        LibrarianList.add(new USERNAME_PWD("Raj", "Kumar"));
+
+        Scanner scn = new Scanner(System.in);
+        boolean ValidInput = false;
+        int EnteredNumber = 0;
+        while (!ValidInput){
+            System.out.println("ENTER '1' FOR LOGIN | TO LOGOUT ENTER '3'");
+            EnteredNumber = scn.nextInt();
+
+            if (EnteredNumber == 1){
+                System.out.println("LOGIN");
+                login(LibrarianList, "Librarian");
+                ValidInput = true;
+            }
+            else if (EnteredNumber == 3){
+                System.out.println("LOGOUT");
+                ValidInput = true;
+            }
+            else {
+                System.out.println("WRONG INPUT !!!");
             }
         }
     }
@@ -62,7 +91,7 @@ public class UserManagement {
 
             if (EnteredNumber == 1){
                 System.out.println("LOGIN");
-                login(personList);
+                login(personList, "User");
                 ValidInput = true;
             }
             else if (EnteredNumber == 2){
@@ -79,7 +108,6 @@ public class UserManagement {
             }
         }
         scn.close();
-
         }
 
     private void register(ArrayList<USERNAME_PWD> arrayList) {
@@ -92,17 +120,17 @@ public class UserManagement {
 
         arrayList.add(new USERNAME_PWD(name, password));
         System.out.println("LOGGING IN ...");
-        login(arrayList);
+        login(arrayList, "User");
 
     }
 
-    private void login(ArrayList<USERNAME_PWD> arrayList) {
+    private void login(ArrayList<USERNAME_PWD> arrayList, String UserOrLib) {
         Scanner scn = new Scanner(System.in);
         boolean UserPresent = false;
-
+        String name;
         do {
             System.out.println("ENTER YOUR NAME");
-            String name = scn.nextLine();
+            name = scn.nextLine();
             System.out.println("ENTER YOUR PASSWORD");
             String password = scn.nextLine();
 
@@ -110,15 +138,24 @@ public class UserManagement {
             if (val.getName().equals(name) && val.getPassword().equals(password)){
                 System.out.println("Present");
                 UserPresent = true;
-            }
+            }else{
             System.out.println("ENTER YOUR CREDENTIALS AGAIN !!!");
             try {
                 TimeUnit.SECONDS.sleep(3);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
+            }
         }
     }while (!UserPresent);
+
+        if(UserPresent && UserOrLib == "Librarian"){
+            Librarian obj = new Librarian(name);
+            // TODO can be changed
+        }
+        else if(UserPresent && UserOrLib == "User"){
+            // TODO call librarian
+        }
 }
 
     public static void main(String[] args) {
